ef226f11fb7b20bf897b046b42012a62a6feeb09
nickby <nick.mail@tut.by>
2016-08-14 06:29:43 +0300

    Убираем старый автозагрузчик модулей

--- components/Autoload.php
+++ components/Autoload.php
@@ -1,22 +1,5 @@
 <?php
 
-//
-//function __autoload($class_name)
-//{
-//    $array_path = array(
-//        '/models/',
-//        '/components/',
-//    );
-//
-//    foreach ($array_path as $path) {
-//        $path = ROOT . $path . $class_name . '.php';
-//        if (is_file($path)) {
-//            include_once($path);
-//        }
-//    }
-//}
-
-
 function my_autoloader($class)
 {
     $array_path = array(
